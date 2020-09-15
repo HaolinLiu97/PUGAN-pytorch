fix bug for loss computation

loss/loss.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import os,sys
 sys.path.append('../')
-from third_party.auction_match import auction_match
+from auction_match import auction_match
 import pointnet2.pointnet2_utils as pn2_utils
 import math
 from knn_cuda import KNN